9a45a46bafda2d172e6178303ce97941e888f775
HungyuSu <hungyu.su@gocharm.com.tw>
2014-04-09 13:34:07 +0800

    drop down icon added

--- appTemplate/ViewController.m
+++ appTemplate/ViewController.m
@@ -67,17 +67,17 @@
      set logout button on right of navigationBar
      */
     _selectedPeriod = 0;
-    UIView *tmpView = [[UIView alloc] initWithFrame:CGRectMake(0.0f, 0.0f, 160.0f, 44.0f)];
-    //UIImageView *imgView = [[UIImageView alloc] initWithFrame:CGRectMake(0.0f, 0.0f, 160.0f, 60.0f)];
-    //[imgView setImage:[UIImage imageNamed:@"title_bg.png"]];
+    UIView *tmpView = [[UIView alloc] initWithFrame:CGRectMake(0.0f, 0.0f, 140.0f, 44.0f)];
+    UIImageView *imgView = [[UIImageView alloc] initWithFrame:CGRectMake(110.0f, 10.0f, 20.0f, 20.0f)];
+    [imgView setImage:[UIImage imageNamed:@"down.png"]];
     _titleLabel = [[UILabel alloc] initWithFrame:CGRectMake(0.0f, 0.0f, 140.0f, 44.0f)];
-    [_titleLabel setTextAlignment:NSTextAlignmentCenter];
+    [_titleLabel setTextAlignment:NSTextAlignmentLeft];
     [_titleLabel setTextColor:[UIColor blackColor]];
     [_titleLabel setFont:[UIFont boldSystemFontOfSize:17]];
     [_titleLabel setTextAlignment:NSTextAlignmentCenter];
     [_titleLabel setTextColor:[UIColor redColor]];
-    [_titleLabel setText: [NSString stringWithFormat:@"%@ ★", [_catPickerValue objectAtIndex:[_catIDArray indexOfObject:_catID]]]];//]]@"一週活動"];
-    //[tmpView addSubview:imgView];
+    [_titleLabel setText: [NSString stringWithFormat:@"%@", [_catPickerValue objectAtIndex:[_catIDArray indexOfObject:_catID]]]];//]]@"一週活動"];
+    [tmpView addSubview:imgView];
     [tmpView addSubview:_titleLabel];
     
     //UIImageView *imageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"Trend"]];
@@ -230,7 +230,7 @@
             _catID = [_catIDArray objectAtIndex:[_picker selectedRowInComponent:0]];
             //[[[self navigationItem] rightBarButtonItem] setTitle:[_catPickerValue objectAtIndex:[_picker selectedRowInComponent:0]]];
             [_titleLabel setTextAlignment:NSTextAlignmentCenter];
-            _titleLabel.text = [NSString stringWithFormat:@"%@ ★", [_catPickerValue objectAtIndex:[_picker selectedRowInComponent:0]]];//∨
+            _titleLabel.text = [NSString stringWithFormat:@"%@", [_catPickerValue objectAtIndex:[_picker selectedRowInComponent:0]]];//∨
             //_titleLabel.text = [_pickerValue objectAtIndex:0];
             [[[self navigationItem] rightBarButtonItem] setTitle:[_pickerValue objectAtIndex:0]];
             _myData = [NSMutableData new];
